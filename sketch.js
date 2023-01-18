fc3d74019d74d5e1ff7ca25da3f05337bdfb4a58
Yizhi Tang <tangyz@umich.edu>
2023-01-17 20:56:32 -0500

    透明shadow

--- sketch.js
+++ sketch.js
@@ -8,16 +8,14 @@ let banana_size = 200;
 let canvas_size = 800;
 let canvas_color = 220;
 //   orbit setting
-let orbit_hori = 211.27 // calculated by 200:18.8 = x : 19.86(this is the length of the tape)
-let orbit_verti = 100
+let orbit_hori = 300 // calculated by 200:18.8 = x : 19.86(this is the length of the tape)
+let orbit_verti = 200
 let accumulate_x = -orbit_hori;
 let speed = 1;
 let flip = 1;
-let rotate_deg = 30;
+let rotate_deg = 40;
 //   shadow setting
-let angle = 0;
-
-let orbitRadius = 200;
+let last_sec = 0;
 
 function setup() {
   createCanvas(canvas_size, canvas_size);
@@ -27,15 +25,6 @@ function setup() {
     bananas.push(loadImage(temp_filename));
   }
   shadow = loadImage('https://raw.githubusercontent.com/tangyizhi2000/BananaTimer/main/banana%20shadow.png');
-}
-
-function rotate_point_around_origin(xloc, yloc, deg) {
-  let points = [];
-  new_xloc = xloc * cos(deg) - yloc * sin(deg);
-  new_yloc = yloc * cos(deg) + xloc * sin(deg);
-  points.push(new_xloc);
-  points.push(new_yloc);
-  return points;
 }
 
 function draw() {
@@ -48,10 +37,11 @@ function draw() {
   // orbit
   //translate(-width / 2, -height / 2);
   var y = sqrt((1 - (accumulate_x * accumulate_x) / (orbit_hori * orbit_hori)) * (orbit_verti * orbit_verti))
-  let points = [];
-  points = rotate_point_around_origin(accumulate_x, y);
-  print(points[0], points[1])
-  ellipse(accumulate_x, y * flip, 30, 30);
+  // rotation
+  new_xloc = accumulate_x * cos(rotate_deg * flip) - y * sin(rotate_deg * flip);
+  new_yloc = y * cos(rotate_deg * flip) + accumulate_x * sin(rotate_deg * flip);
+  // loc
+  ellipse(new_xloc, new_yloc * flip, 20, 20);
   if (accumulate_x + speed > orbit_hori){
     flip = -1;
   } else if (accumulate_x - speed < -orbit_hori){
@@ -62,5 +52,12 @@ function draw() {
   } else if (flip == -1) {
     accumulate_x -= speed;
   }
-  
+  // shadow
+  //rotate_angle = (60 - second()) * 6
+  rotate(second() - last_sec * 6);
+  print(second() * 6);
+  imageMode(CENTER);
+  distance_to_orbit = sqrt(new_xloc * new_xloc + new_yloc * new_yloc);
+  image(shadow, 0, 0, distance_to_orbit / 1104 * 533 * 2, distance_to_orbit * 2);
+  last_sec = second();
 }
